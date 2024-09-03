f2a746ca26efd296e6495013d90be6e1da0c20aa
derGraph <paulantongraf@gmail.com>
2024-09-03 17:15:54 +0200

    changed from polka to express for compression

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { handler } from './handler.js';
-import polka from 'polka';
-import compression from '@polka/compression';
+import express from 'express';
+import compression from 'compression';
 
-let server = polka();
-
-server.use(handler);
+let server = express();
 server.use(compression({
 	gzip: true,
-	brotli: true,
-	threshold: 1024
-}))
+	threshold: 5120,
+	level: 9
+}));
+server.use(handler);
+
 
 server.listen(3000, () => {
 	console.log('listening on port 3000');
