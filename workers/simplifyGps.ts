dfcefc13e6753437a4bae5177b6827072bd00a4b
derGraph <paulantongraf@gmail.com>
2024-09-07 01:47:03 +0200

    change simply parameters

--- workers/simplifyGps.ts
+++ workers/simplifyGps.ts
@@ -45,7 +45,7 @@ export async function simplifyGps(trip: string, amount: number) {
 				{ lat: Number(lastPoint.lat), lng: Number(lastPoint.long) }
 			);
 	
-			if (crosstrackError < 15 && Math.abs(turnRate) < 40 && distFromLastPoint < 1000) {
+			if (crosstrackError < 10 && Math.abs(turnRate) < 40 && distFromLastPoint < 100) {
 				// Delete Datapoint
 				deletedPoints.push(inputData[i].id);
 			} else {
