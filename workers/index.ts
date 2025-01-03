7abc8195f9102e9ccbfdff3d752c9d11b370db09
derGraph <paulantongraf@gmail.com>
2025-01-03 22:23:10 +0100

    remove unnecessary log

--- workers/index.ts
+++ workers/index.ts
@@ -4,7 +4,6 @@ import { loadGeoJSON, simplify } from "./simplifyGps";
 await loadGeoJSON("store/regionData.geoJson");
 
 while(true){
-    console.log("HI");
     await simplify();
     await calculateUsers();
     await new Promise(f => setTimeout(f, 1000));
