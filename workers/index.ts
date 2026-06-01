21fc951cbbe59a1b13b0f29496ff410175ddf974
derGraph <paulantongraf@gmail.com>
2026-06-01 20:46:51 +0200

    try new worker

--- workers/index.ts
+++ workers/index.ts
@@ -1,10 +1,12 @@
-import { calculateUsers } from "./calculateUser";
-import { loadGeoJSON, simplify } from "./simplifyGps";
+import { calculateUsers } from "./calculateUser.js";
+import { locateImages } from "./locateImages.js";
+import { loadGeoJSON, simplify } from "./simplifyGps.js";
 
 await loadGeoJSON("store/regionData.geoJson");
 
 while(true){
     await simplify();
     await calculateUsers();
+    await locateImages();
     await new Promise(f => setTimeout(f, 1000));
 }
