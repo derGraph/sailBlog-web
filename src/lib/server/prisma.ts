92bdd0e6bab43b7345f7955d02c3bc2debf6573a
derGraph <paulantongraf@gmail.com>
2026-07-05 22:43:10 +0200

    switch to prisma 7

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../../prisma/generated/client'; 
 
-const prisma = new PrismaClient();
+// Initialize the driver adapter with your connection credentials
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST || 'localhost',
+  user: process.env.DATABASE_USER || 'prisma',
+  password: process.env.DATABASE_PASSWORD || 'sailBlog' ,
+  database: process.env.DATABASE_NAME || 'db',
+  port: Number(process.env.DATABASE_PORT) || 3306,
+  connectionLimit: 5, // Recommended for connection pooling in Prisma 7
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
