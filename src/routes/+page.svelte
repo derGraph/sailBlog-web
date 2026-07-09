e6da17bc36fe26914d1de51440d102eee4500eb8
derGraph <paulantongraf@gmail.com>
2026-07-09 02:06:34 +0200

    show all trips on homescreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import type { LatLngExpression } from 'leaflet';
   import Leaflet from '$lib/Leaflet.svelte';
   import { onMount } from 'svelte';
+  import errorStore from '$lib/errorStore.js';
 
   let { data } = $props();
   let tracks: String[] | null = $state(null);
@@ -14,6 +15,26 @@
     console.log('[Home] activeTripId effect', { user: data.user?.username, activeTripId });
     if (activeTripId) {
       tracks = [activeTripId];
+      fetch('/api/Trips?deleted=false').then(async (response) => {
+        if (!response.ok) {
+          $errorStore = response;
+          return;
+        }
+        let allTrips:any[] = await response.json();
+        allTrips = allTrips.filter((trip) => {
+            // If showMyTrips is false, include all trips; otherwise, filter by the user's trips
+            const isMyTrip =
+              trip.crew.some(
+                (crewMember: { username: string | undefined }) =>
+                  crewMember.username === data.user?.username
+              ) ||
+              trip.skipperName === data.user?.username;
+
+          // Combine all conditions
+          return isMyTrip;
+        });
+        tracks = allTrips.map(trip => String(trip.id));
+    });
     } else {
       tracks = null;
     }
@@ -21,5 +42,5 @@
 </script>
 
 <div class="md:container md:mx-auto py-3 h-full rounded">
-  <Leaflet view={initialView} zoom={8} {tracks} showTripImages={true} />
+  <Leaflet view={initialView} zoom={8} {tracks} showTripImages={false} />
 </div>
