df71bd4076b04f9754f3af973e2420798f761eef
derGraph <68235499+derGraph@users.noreply.github.com>
2026-07-28 19:58:50 +0200

    Update prisma.ts

--- workers/prisma.ts
+++ workers/prisma.ts
@@ -8,7 +8,8 @@ const adapter = new PrismaMariaDb({
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME || 'db',
   port: Number(process.env.DATABASE_PORT) || 3306,
-  connectionLimit: 5
+  connectionLimit: 5,
+  allowPublicKeyRetrieval: true
 });
 
 const prisma = new PrismaClient({ adapter });
